fa797cf00e9b9e1ac5dbb6b87be9c2affde27f39
eastZoo <dongju010@naver.com>
2025-02-08 17:23:34 +0900

    UPDATE : 소켓path 설정

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 
 // autoConnect 옵션을 true로 하여 애플리케이션 시작 시 연결하도록 합니다.
 const socket = io(process.env.REACT_APP_API_URL || "http://localhost:3001", {
+  path: "/socket.io",
   transports: ["websocket"],
   autoConnect: true,
 });
