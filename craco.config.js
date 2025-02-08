10c73aa251fb80e6c0e0887d51001f177601d9bf
eastZoo <dongju010@naver.com>
2025-02-08 16:05:25 +0900

    UPDATE : craco 설정파일 업데이트

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,4 @@
+const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
@@ -6,5 +7,12 @@ module.exports = {
       "@": path.resolve(__dirname, "src/"),
       "@components": path.resolve(__dirname, "src/components"),
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.CRACO_ENV || "development"),
+      }),
+    ],
+
   },
+
 };
