ceb4b2057bb98ef5064418ce1a3e0075f08a95d7
eastZoo <dongju010@naver.com>
2025-02-09 00:25:19 +0900

    UPDATE : 모바일에서 포커스 잃는 것 수정

--- src/components/MessageInput/MessageInput.tsx
+++ src/components/MessageInput/MessageInput.tsx
@@ -1,12 +1,8 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import socket from "@/api/socket";
 import { adminInfoSelector } from "@/state/adminInfo";
-import {
-  InputContainer,
-  TextInput,
-  SendButton,
-} from "@/components/MessageInput/MessageInput.styles";
+import { InputContainer, TextInput, SendButton } from "./MessageInput.styles";
 
 interface MessageInputProps {
   chatId: string;
@@ -15,7 +11,10 @@ interface MessageInputProps {
 const MessageInput: React.FC<MessageInputProps> = ({ chatId }) => {
   const [content, setContent] = useState("");
   const adminInfo = useRecoilValue(adminInfoSelector);
+  // 입력 필드에 대한 ref 생성
+  const inputRef = useRef<HTMLInputElement>(null);
 
+  // 메시지 전송 후 입력 필드에 포커스를 유지
   const handleSubmit = useCallback(
     (e: React.FormEvent) => {
       e.preventDefault();
@@ -27,6 +26,10 @@ const MessageInput: React.FC<MessageInputProps> = ({ chatId }) => {
           username: adminInfo.username,
         });
         setContent("");
+        // 메시지 전송 후 입력 필드에 다시 포커스 설정
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
       }
     },
     [content, chatId, adminInfo]
@@ -36,9 +39,17 @@ const MessageInput: React.FC<MessageInputProps> = ({ chatId }) => {
     setContent(e.target.value);
   }, []);
 
+  // 컴포넌트 마운트 시 포커스를 설정하여 키보드가 바로 올라오도록 할 수도 있습니다.
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
     <InputContainer onSubmit={handleSubmit}>
       <TextInput
+        ref={inputRef} // ref 연결
         type="text"
         placeholder="메시지를 입력하세요..."
         value={content}
