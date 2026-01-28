0f1098ae8fc924bcfc6a318d7bc429bf1532a399
JIHYE KIM <kimjeje15@gmail.com>
2026-01-28 14:46:59 +0900

    UPDATE : env 키값 변경

--- src/lib/settingFCM.ts
+++ src/lib/settingFCM.ts
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_API_KEY,
-  authDomain: import.meta.env.VITE_API_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_API_PROJECT_ID,
-  messagingSenderId: import.meta.env.VITE_API_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_API_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 if (!firebaseConfig.apiKey) {
